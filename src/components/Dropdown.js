9d0653216bc31d0041e11a2d0c218ac92cc7664c
Monsalvo Geoffrey <monsalvo.geoffrey@gmail.com>
2024-02-20 10:02:44 +0100

    Remove unnecessary state in Dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -3,17 +3,11 @@ import '../styles/Dropdown.css'
 import arrow_back from '../assets/arrow_back.png'
 
 function Dropdown({ title, children }) {
-    let [dropdownClass, setDropdownClass] = useState("dropdown dropdown--open")
     let [open, setOpen] = useState(true);
 
-    function toggle() {
-        setDropdownClass(!open ? "dropdown dropdown--open" : "dropdown")
-        setOpen(!open)
-    }
-
     return (
-        <div className={dropdownClass}>
-            <p className="dropdown-title" onClick={toggle} role="button"><span>{title}</span>
+        <div className={open === true ? "dropdown dropdown--open" : "dropdown"}>
+            <p className="dropdown-title" onClick={() => setOpen(!open)} role="button"><span>{title}</span>
                 <img src={arrow_back} alt="" className="dropdown-image" /></p>
             <p className="dropdown-content">{children}</p>
         </div>
